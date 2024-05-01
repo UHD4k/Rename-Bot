34affc00f53a0498598907e39c2590e612952bd1
UHD4K <148746604+UHD4k@users.noreply.github.com>
2024-05-01 14:07:41 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -4,26 +4,26 @@ from helper.database import AshutoshGoswami24
 @Client.on_message(filters.private & filters.command(['set_caption', "sc"]))
 async def add_caption(client, message):
     if len(message.command) == 1:
-       return await message.reply_text("**Give The Caption\n\nExample :- `/set_caption 📕Name ➠ : {filename} \n\n🔗 Size ➠ : {filesize} \n\n⏰ Duration ➠ : {duration}`**")
+       return await message.reply_text("**Gɪᴠᴇ Tʜᴇ Cᴀᴘᴛɪᴏɴ\n\nExᴀᴍᴘʟᴇ :- `/set_caption 📕 Nᴀᴍᴇ ➠ : {filename} \n\n🔗 Sɪᴢᴇ ➠ : {filesize} \n\n⏰ Dᴜʀᴀᴛɪᴏɴ ➠ : {duration}`**")
     caption = message.text.split(" ", 1)[1]
     await AshutoshGoswami24.set_caption(message.from_user.id, caption=caption)
-    await message.reply_text("**Your Caption Successfully Added ✅**")
+    await message.reply_text("**Yᴏᴜʀ Cᴀᴘᴛɪᴏɴ Sᴜᴄᴄᴇssғᴜʟʟʏ Aᴅᴅᴇᴅ ✅**")
    
 @Client.on_message(filters.private & filters.command(['del_caption', "dc"]))
 async def delete_caption(client, message):
     caption = await AshutoshGoswami24.get_caption(message.from_user.id)  
     if not caption:
-       return await message.reply_text("**You Don't Have Any Caption ❌**")
+       return await message.reply_text("**Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴʏ Cᴀᴘᴛɪᴏɴ ❌**")
     await AshutoshGoswami24.set_caption(message.from_user.id, caption=None)
-    await message.reply_text("**Your Caption Successfully Deleted 🗑️**")
+    await message.reply_text("**Yᴏᴜʀ Cᴀᴘᴛɪᴏɴ Sᴜᴄᴄᴇssғᴜʟʟʏ Dᴇʟᴇᴛᴇᴅ 🗑️**")
                                        
 @Client.on_message(filters.private & filters.command(['see_caption', 'view_caption', "vc"]))
 async def see_caption(client, message):
     caption = await AshutoshGoswami24.get_caption(message.from_user.id)  
     if caption:
-       await message.reply_text(f"**Your Caption :**\n\n`{caption}`")
+       await message.reply_text(f"**Yᴏᴜʀ Cᴀᴘᴛɪᴏɴ :**\n\n`{caption}`")
     else:
-       await message.reply_text("**You Don't Have Any Caption ❌**")
+       await message.reply_text("**Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴʏ Cᴀᴘᴛɪᴏɴ ❌**")
 
 
 
